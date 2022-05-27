9781fef0adc62ea5d1f74ce5fa181eb0630f4747
JunHyung Cha <41867381+JunHCha@users.noreply.github.com>
2022-05-27 11:20:31 +0900

    Upcoming/Search View Controller 구현 (#1)
    * feat: add upcoming/search view controller
    
    * docs: fix readme
    
    * Update README.md

---
+++ Netflix Clone IOS/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone IOS
+//
+//  Created by 차준형 on 2022/05/26.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
