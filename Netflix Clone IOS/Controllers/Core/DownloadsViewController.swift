4abadba4c5f410a5d652b40cc32c56010ceaf1c4
JunHCha <quffl9612@naver.com>
2022-06-04 18:33:05 +0900

    Add search results view, preview view

---
+++ Netflix Clone IOS/Controllers/Core/DownloadsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DownloadsViewController.swift
+//  Netflix Clone IOS
+//
+//  Created by 차준형 on 2022/05/22.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
--- Netflix Clone IOS/Managers/APICaller.swift
+++ Netflix Clone IOS/Managers/APICaller.swift
@@ -8,8 +8,10 @@
 import Foundation
 
 struct Constants {
-    static let API_KEY = ""
+    static let API_KEY = Bundle.main.infoDictionary?["API_KEY"] as? String ?? ""
     static let baseURL = "https://api.themoviedb.org"
+    static let YoutubeAPI_KEY = Bundle.main.infoDictionary?["YoutubeAPI_KEY"] as? String ?? ""
+    static let YoutubeBaseURL = "https://youtube.googleapis.com/youtube/v3/search?"
 }
 
 enum APIError: Error {
@@ -113,7 +115,7 @@ class APICaller {
     }
     
     func getDiscoverMovies(completion: @escaping (Result<[Title], Error>) -> Void)  {
-        // https://api.themoviedb.org/
+
         guard let url = URL(string: "\(Constants.baseURL)/3/discover/movie?api_key=\(Constants.API_KEY)&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate") else {return}
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {data, _, error in guard let data = data, error == nil else {
             return
@@ -126,6 +128,51 @@ class APICaller {
             }
             catch {
                 completion(.failure(APIError.failedTogetData))
+
+            }
+        }
+        task.resume()
+    }
+    
+    func search(with query: String, completion: @escaping (Result<[Title], Error>) -> Void)  {
+
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {return}
+        
+        guard let url = URL(string: "\(Constants.baseURL)/3/search/movie?api_key=\(Constants.API_KEY)&query=\(query)") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {data, _, error in guard let data = data, error == nil else {
+            return
+        }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+                completion(.success(results.results))
+
+            }
+            catch {
+                completion(.failure(APIError.failedTogetData))
+
+            }
+        }
+        task.resume()
+    }
+    
+    func getMovie(with query: String, completion: @escaping (Result<VideoElement, Error>) -> Void) {
+        
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {return}
+        guard let url = URL(string: "\(Constants.YoutubeBaseURL)q=\(query)&key=\(Constants.YoutubeAPI_KEY)") else {return}
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {data, _, error in guard let data = data, error == nil else {
+            return
+        }
+            
+            do {
+                let results = try JSONDecoder().decode(YoutubeSearchResponse.self, from: data)
+                completion(.success(results.items[0]))
+            }
+            catch {
+                completion(.failure(error))
+                print(error.localizedDescription)
 
             }
         }
